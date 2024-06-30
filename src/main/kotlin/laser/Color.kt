c63775f809ab626e5a9d019a8087006f19b3b138
Daniel Thirion <defvs.daniel@gmail.com>
2024-06-30 13:07:50 +0200

    Refactor LaserObject.kt

---
+++ src/main/kotlin/laser/Color.kt
@@ -0,0 +1,8 @@
+package laser
+
+data class Color(
+    val red: Double,
+    val green: Double,
+    val blue: Double,
+    val opacity: Double = 1.0,
+)
--- src/main/kotlin/laser/LaserObject.kt
+++ src/main/kotlin/laser/LaserObject.kt
@@ -1,36 +1,5 @@
 package laser
 
-import kotlin.math.cos
-import kotlin.math.sin
-
-data class Point(
-    var x: Double,
-    var y: Double,
-) {
-    fun offset(x: Double, y: Double) {
-        this.x += x
-        this.y += y
-    }
-
-    fun rotate(angle: Double, anchorX: Double, anchorY: Double) {
-        if (angle % 1.0 == 0.0) return
-        val radians = angle * 2 * Math.PI
-        val translatedX = this.x - anchorX
-        val translatedY = this.y - anchorY
-        val rotatedX = translatedX * cos(radians) - translatedY * sin(radians)
-        val rotatedY = translatedX * sin(radians) + translatedY * cos(radians)
-        this.x = rotatedX + anchorX
-        this.y = rotatedY + anchorY
-    }
-}
-
-data class Color(
-    val red: Double,
-    val green: Double,
-    val blue: Double,
-    val opacity: Double = 1.0,
-)
-
 abstract class LaserObject(val points: List<Point>)
 class LaserPoint(val point: Point, val color: Color) : LaserObject(points = listOf(point))
 class LaserPolygon(points: List<Point>, closed: Boolean) : LaserObject(points) // todo colors
