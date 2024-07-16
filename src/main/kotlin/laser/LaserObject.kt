b6dd86bcd939be7311d6fb51194d8fdf45ef4d26
Daniel Thirion <defvs.daniel@gmail.com>
2024-07-16 14:02:39 +0200

    Document 2D APIs

--- src/main/kotlin/laser/LaserObject.kt
+++ src/main/kotlin/laser/LaserObject.kt
@@ -3,23 +3,92 @@ package laser
 import com.github.ajalt.colormath.model.HSL
 import com.github.ajalt.colormath.model.RGB
 
+/**
+ * Represents a laser object, which may consist of multiple laser points.
+ *
+ * @property laserPoints A list of LaserPoint objects.
+ * @property closedPolygon Indicates if the points form a closed polygon.
+ */
 class LaserObject(val laserPoints: List<LaserPoint>, val closedPolygon: Boolean) {
+    /**
+     * Retrieves the points from the laser points.
+     */
     val points: List<Point>
         get() = laserPoints.map { it.point }
 
+    /**
+     * Constructs a LaserObject with a single point and color.
+     *
+     * @param point The point.
+     * @param color The color of the laser point.
+     */
     constructor(point: Point, color: RGB) : this(listOf(LaserPoint(point, color)), false)
 
+    /**
+     * Represents a single laser point with color information.
+     *
+     * @property point The point.
+     * @property colors The colors associated with the point.
+     * @property gradientType The gradient type.
+     */
     class LaserPoint(val point: Point, val colors: ArrayList<RGB>, var gradientType: GradientType) {
         constructor(point: Point, color: RGB) : this(point, arrayListOf(color), GradientType.None)
     }
 
+    /**
+     * Applies a position transformation to all points.
+     *
+     * @param transform The transformation function to apply.
+     */
     fun applyPositionTransform(transform: Point.() -> Unit) = points.forEach(transform)
+
+    /**
+     * Transforms the RGB color of all laser points.
+     *
+     * @param transform The transformation function to apply.
+     */
     fun transformColorRGB(transform: (RGB) -> RGB) =
         laserPoints.forEach { laserPoint -> laserPoint.colors.replaceAll { transform(it) } }
+
+    /**
+     * Transforms the HSL color of all laser points.
+     *
+     * @param transform The transformation function to apply.
+     */
     fun transformColorHSL(transform: (HSL) -> HSL) =
         laserPoints.forEach { laserPoint -> laserPoint.colors.replaceAll { transform(it.toHSL()).toSRGB() } }
+
+    /**
+     * Scales the polygon by a given factor with respect to an anchor point.
+     *
+     * @param factor The scaling factor.
+     * @param anchorX The x-coordinate of the anchor point.
+     * @param anchorY The y-coordinate of the anchor point.
+     */
+    fun scale(factor: Float, anchorX: Float, anchorY: Float) {
+        applyPositionTransform {
+            this.x = anchorX + (this.x - anchorX) * factor
+            this.y = anchorY + (this.y - anchorY) * factor
+        }
+    }
+
+    /**
+     * Rotates the polygon by a given angle around an anchor point.
+     *
+     * @param angle The angle in degrees.
+     * @param anchorX The x-coordinate of the anchor point.
+     * @param anchorY The y-coordinate of the anchor point.
+     */
+    fun rotate(angle: Float, anchorX: Float, anchorY: Float) {
+        applyPositionTransform {
+            this.rotate(angle, anchorX, anchorY)
+        }
+    }
 }
 
+/**
+ * Enum representing the gradient type of a laser point.
+ */
 enum class GradientType {
     Smooth, Steps, None,
 }
--- src/main/kotlin/laser/Point.kt
+++ src/main/kotlin/laser/Point.kt
@@ -2,24 +2,84 @@ package laser
 
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.random.Random
 
+/**
+ * Represents a 2D point.
+ *
+ * @property x The x-coordinate of the point.
+ * @property y The y-coordinate of the point.
+ */
 data class Point(
     var x: Float,
     var y: Float,
 ) {
+    /**
+     * Offsets the point by the given x and y values.
+     *
+     * @param x The x-offset.
+     * @param y The y-offset.
+     */
     fun offset(x: Float, y: Float) {
         this.x += x
         this.y += y
     }
 
+    /**
+     * Rotates the point around a given anchor point by a specified angle.
+     *
+     * @param angle The angle in degrees.
+     * @param anchorX The x-coordinate of the anchor point.
+     * @param anchorY The y-coordinate of the anchor point.
+     */
     fun rotate(angle: Float, anchorX: Float, anchorY: Float) {
         if (angle % 1.0 == 0.0) return
-        val radians = angle * 2 * Math.PI
+        val radians = angle * 2 * Math.PI / 360.0
         val translatedX = this.x - anchorX
         val translatedY = this.y - anchorY
         val rotatedX = translatedX * cos(radians) - translatedY * sin(radians)
         val rotatedY = translatedX * sin(radians) + translatedY * cos(radians)
         this.x = (rotatedX + anchorX).toFloat()
         this.y = (rotatedY + anchorY).toFloat()
+    }
+
+    /**
+     * Reflects the point across the X-axis.
+     */
+    fun axialSymmetryX() {
+        this.y = -this.y
+    }
+
+    /**
+     * Reflects the point across the Y-axis.
+     */
+    fun axialSymmetryY() {
+        this.x = -this.x
+    }
+
+    /**
+     * Offsets the point randomly within the given bounds.
+     *
+     * @param maxX The maximum x-offset.
+     * @param maxY The maximum y-offset.
+     */
+    fun randomOffset(maxX: Float, maxY: Float) {
+        this.x += Random.nextFloat() * maxX - maxX / 2
+        this.y += Random.nextFloat() * maxY - maxY / 2
+    }
+
+    /**
+     * Reflects the point across an arbitrary axis passing through the origin.
+     *
+     * @param angle The angle in degrees defining the axis from the positive x-axis.
+     */
+    fun axialSymmetry(angle: Float) {
+        val radians = angle * 2 * Math.PI / 360.0
+        val cosTheta = cos(radians)
+        val sinTheta = sin(radians)
+        val newX = this.x * cosTheta + this.y * sinTheta
+        val newY = this.x * sinTheta - this.y * cosTheta
+        this.x = (newX * cosTheta + newY * sinTheta).toFloat()
+        this.y = (newX * sinTheta - newY * cosTheta).toFloat()
     }
 }
