74c9161a75c7521eed64faa1af7581d10d2b2895
Daniel Thirion <defvs.daniel@gmail.com>
2024-08-21 20:50:50 +0200

    Add confirmation on EditorWindow close if there are unsaved changes

--- src/main/kotlin/ui/editor/EditorWindow.kt
+++ src/main/kotlin/ui/editor/EditorWindow.kt
@@ -7,9 +7,7 @@ import helpers.serialization.json
 import javafx.application.Platform
 import javafx.beans.binding.Bindings
 import javafx.scene.Scene
-import javafx.scene.control.Menu
-import javafx.scene.control.MenuBar
-import javafx.scene.control.MenuItem
+import javafx.scene.control.*
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyCodeCombination
 import javafx.scene.input.KeyCombination
@@ -18,6 +16,7 @@ import javafx.stage.FileChooser
 import javafx.stage.Stage
 import kotlinx.serialization.encodeToString
 import java.io.File
+import kotlin.jvm.optionals.getOrNull
 
 class EditorWindow private constructor(private val clip: Clip, private var initiallyEmpty: Boolean = false) {
     private val editorPane = EditorPane(clip)
@@ -25,8 +24,7 @@ class EditorWindow private constructor(private val clip: Clip, private var initi
 
     fun createAndShow() {
         Platform.runLater {
-            stage = Stage()
-            with(stage) {
+            stage = Stage().apply {
                 val menuBar = MenuBar().apply {
                     menus += Menu("File", null,
                         MenuItem("Save").apply {
@@ -56,6 +54,11 @@ class EditorWindow private constructor(private val clip: Clip, private var initi
                     if (editorPane.nodeCompositor.hasUnsavedChanges.get()) "• ${clip.name.value}" else clip.name.value
                 }, clip.name, editorPane.nodeCompositor.hasUnsavedChanges))
 
+                setOnCloseRequest { event ->
+                    event.consume()
+                    this@EditorWindow.close()
+                }
+
                 show()
 
                 if (initiallyEmpty)
@@ -64,25 +67,62 @@ class EditorWindow private constructor(private val clip: Clip, private var initi
         }
     }
 
-    private fun saveClip() {
+    private fun close() {
+        if (editorPane.nodeCompositor.hasUnsavedChanges.get()) {
+            val alert = Alert(Alert.AlertType.CONFIRMATION).apply {
+                title = "Unsaved Changes"
+                headerText = "You have unsaved changes."
+                contentText = "Do you want to save your changes before closing?"
+
+                buttonTypes.setAll(ButtonType("Save"), ButtonType("Don't Save"), ButtonType.CANCEL)
+            }
+
+            val result = alert.showAndWait()
+
+            when (result.getOrNull()?.text) {
+                "Save" -> {
+                    if (saveClip()) // make sure saving goes as planned
+                        stage.close()
+                }
+
+                "Don't Save" -> {
+                    stage.close()
+                }
+
+                else -> {
+                    // Do nothing, just return to the editor
+                }
+            }
+        } else {
+            stage.close()
+        }
+    }
+
+    private fun saveClip(): Boolean {
         if (initiallyEmpty) {
             initiallyEmpty = false
             return saveClipAs()
         }
-        File("${clip.name.value}.json").writeText(json.encodeToString<Clip>(clip))
-        editorPane.nodeCompositor.hasUnsavedChanges.set(false)
+        runCatching { File("${clip.name.value}.json").writeText(json.encodeToString<Clip>(clip)) }
+            .onFailure {
+                Alert(Alert.AlertType.ERROR, "Failed to save to file!", ButtonType.CLOSE)
+                return false
+            }
+            .onSuccess { editorPane.nodeCompositor.hasUnsavedChanges.set(false) }
+        return true
     }
 
-    private fun saveClipAs() {
-        val fileChooser = FileChooser()
-        fileChooser.extensionFilters.add(FileChooser.ExtensionFilter("JSON Clip Files", "*.json"))
-        val selectedFile: File? = fileChooser.showSaveDialog(stage)
-        selectedFile?.let { file ->
-            clip.name.set(file.nameWithoutExtension)
-            clip.uuid.set(ClipUUID())
-            saveClip()
-        }
-    }
+    /**
+     * @return true if a file was picked, false if not.
+     */
+    private fun saveClipAs() = FileChooser().apply {
+        extensionFilters.add(FileChooser.ExtensionFilter("JSON Clip Files", "*.json"))
+    }.showSaveDialog(stage)?.let { file ->
+        clip.name.set(file.nameWithoutExtension)
+        clip.uuid.set(ClipUUID())
+        saveClip()
+        true
+    } ?: false
 
     companion object {
         fun openFromFile() {
